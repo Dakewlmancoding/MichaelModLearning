c5ef22d48945dd71d3e68ec278f3b6a7dae53050
Jack Bell <jack@bellhastold.com>
2025-06-03 11:00:43 -0400

    Added a Banana! --Jack B.

---
+++ src/main/java/com/example/examplemod/Items/ModItems.java
@@ -0,0 +1,18 @@
+package com.example.examplemod.Items;
+
+import com.example.examplemod.ExampleMod;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ExampleMod.MODID);
+
+    public static final DeferredItem<Item> BANANA = ITEMS.register("banana", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+}
